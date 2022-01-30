7ca3bd3d0063d5922c5ae7e8bcd701d9d26f34f2
Evie <evita@stenqvist.co>
2022-01-30 10:54:28 +0100

    Landing info fixed for smaller screens

--- assets/js/bankless.js
+++ assets/js/bankless.js
@@ -1,4 +1,7 @@
 function toggleDarkMode() {
+  const wasDarkmode = localStorage.getItem('darkmode') === 'true';
+  localStorage.setItem('darkmode', !wasDarkmode);
+
   document.querySelector("#sun").classList.toggle("active");
   document.querySelector("#moon").classList.toggle("active");
   document.documentElement.classList.toggle("dark-mode");
